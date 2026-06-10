cb1d97c5ee712c874828942eac767c7f386acfc9
sidneyamorim1 <sid.amorim1@gmail.com>
2026-06-10 14:38:56 -0300

    Rename portal to Megamania Remake

--- portal.js
+++ portal.js
@@ -2,13 +2,13 @@
   const playLink = document.getElementById('playLink');
   const statusText = document.getElementById('statusText');
   const gameFrame = document.getElementById('gameFrame');
-  statusText.textContent = 'Prévia carregando';
+  statusText.textContent = 'Megamania-Remake carregando';
 
   playLink.addEventListener('click', () => {
     statusText.textContent = 'Abrindo o jogo';
   });
 
   gameFrame.addEventListener('load', () => {
-    statusText.textContent = 'Prévia carregada';
+    statusText.textContent = 'Megamania-Remake carregado';
   });
 })();
